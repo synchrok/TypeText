905dc1170943dc611d077509b1511be8f985f8f8
synchrok <synchrok@awesomepiece.com>
2015-12-12 18:35:22 +0900

    ugui add

---
+++ uGUI/Assets/Test_UGUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Test_UGUI : MonoBehaviour {
+
+    public Text text;
+    private Queue<string> scripts = new Queue<string>();
+
+    public void Start() {
+        scripts.Enqueue("Hello! My name is[speed=0.2]... [speed=0.4]NPC");
+        scripts.Enqueue("You can <b>use</b> <i>uGUI</i> <size=40>text</size> <size=20>tag</size> and <color=#ff0000ff>color</color> tag <color=#00ff00ff>like this</color>.");
+        scripts.Enqueue("bold <b>text</b> test <b>bold</b> text <b>test</b>");
+        scripts.Enqueue("You can <size=40>size 40</size> and <size=20>size 20</size>");
+        scripts.Enqueue("You can <color=#ff0000ff>color</color> tag <color=#00ff00ff>like this</color>.");
+        ShowScript();
+    }
+
+    private void ShowScript() {
+        if (scripts.Count <= 0)
+            return;
+        text.TypeText(scripts.Dequeue());
+    }
+
+    public void OnClickWindow() {
+        if (text.IsSkippable()) {
+            text.SkipTypeText();
+        } else {
+            ShowScript();
+        }
+    }
+
+}
---
+++ uGUI/Assets/TypeTextComponent.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 한글자씩 입력되는 효과를 구현해주는 컴포넌트
+[RequireComponent(typeof(Text))]
+public class TypeTextComponent : MonoBehaviour {
+
+    [SerializeField]
+    private float _defaultSpeed = 0.05f;
+
+    private Text label;
+    private string _currentText;
+    private string _finalText;
+    private Coroutine _typeTextCoroutine;
+
+    private static readonly string[] _uguiSymbols = { "b", "i" }; 
+    private static readonly string[] _uguiCloseSymbols = { "b", "i", "size", "color" }; 
+
+    private void Init() {
+        if (label == null)
+            label = GetComponent<Text>();
+    }
+
+    public void Awake() {
+        Init();
+    }
+
+    public void SetText(string text, float speed = -1) {
+        Init();
+
+        _defaultSpeed = speed > 0 ? speed : _defaultSpeed;
+        _finalText = ReplaceSpeed(text);
+        label.text = "";
+
+        if (_typeTextCoroutine != null) {
+            StopCoroutine(_typeTextCoroutine);
+        }
+
+        _typeTextCoroutine = StartCoroutine(TypeText(text));
+    }
+
+    public void SkipTypeText() {
+        StopCoroutine(_typeTextCoroutine);
+        _typeTextCoroutine = null;
+        label.text = _finalText;
+    }
+
+    // 속도 조절을 지원함 (기본속도는 0.05s)
+    // 예) [speed=0.5]안녕하세요? [speed=0.05]저는 전우성입니다. ㅋㅋ
+
+    // 색상 태그도 지원함
+    // 예) 안녕하세요 [FFFF00]우유들컹[-]입니다. [ff0000]반가워요![-]ㅎㅎ고쳤냐?
+
+    public IEnumerator TypeText(string text) {
+        _currentText = "";
+
+        var len = text.Length;
+        var speed = _defaultSpeed;
+        var tagOpened = false;
+        var tagType = "";
+        for (var i = 0; i < len; i++) {
+            if (text[i] == '[' && i + 6 < len && text.Substring(i, 7).Equals("[speed=")) {
+                var parseSpeed = "";
+                for (var j = i + 7; j < len; j++) {
+                    if (text[j] == ']')
+                        break;
+                    parseSpeed += text[j];
+                }
+
+                if (!float.TryParse(parseSpeed, out speed))
+                    speed = 0.05f;
+
+                i += 8 + parseSpeed.Length - 1;
+                continue;
+            }
+
+            // ngui color tag
+            if (text[i] == '[' && i + 7 < len && text[i + 7] == ']') {
+                _currentText += text.Substring(i, 8);
+                i += 8 - 1;
+                continue;
+            }
+
+            var symbolDetected = false;
+            for (var j = 0; j < _uguiSymbols.Length; j++) {
+                var symbol = string.Format("<{0}>", _uguiSymbols[j]);
+                if (text[i] == '<' && i + (1 + _uguiSymbols[j].Length) < len && text.Substring(i, 2 + _uguiSymbols[j].Length).Equals(symbol)) {
+                    _currentText += symbol;
+                    i += (2 + _uguiSymbols[j].Length) - 1;
+                    symbolDetected = true;
+                    tagOpened = true;
+                    tagType = _uguiSymbols[j];
+                    break;
+                }
+            }
+
+            if (text[i] == '<' && i + (1 + 15) < len && text.Substring(i, 2 + 6).Equals("<color=#") && text[i+16] == '>') {
+                _currentText += text.Substring(i, 2 + 6 + 8);
+                i += (2 + 14) - 1;
+                symbolDetected = true;
+                tagOpened = true;
+                tagType = "color";
+            }
+
+            if (text[i] == '<' && i + 5 < len && text.Substring(i, 6).Equals("<size=")) {
+                var parseSize = "";
+                var size = (float) label.fontSize;
+                for (var j = i + 6; j < len; j++) {
+                    if (text[j] == '>') break;
+                    parseSize += text[j];
+                }
+
+                if (float.TryParse(parseSize, out size)) {
+                    _currentText += text.Substring(i, 7 + parseSize.Length);
+                    i += (7 + parseSize.Length) - 1;
+                    symbolDetected = true;
+                    tagOpened = true;
+                    tagType = "size";                    
+                }
+            }
+
+            // exit symbol
+            for (var j = 0; j < _uguiCloseSymbols.Length; j++) {
+                var symbol = string.Format("</{0}>", _uguiCloseSymbols[j]);
+                if (text[i] == '<' && i + (2 + _uguiCloseSymbols[j].Length) < len && text.Substring(i, 3 + _uguiCloseSymbols[j].Length).Equals(symbol)) {
+                    _currentText += symbol;
+                    i += (3 + _uguiCloseSymbols[j].Length) - 1;
+                    symbolDetected = true;
+                    tagOpened = false;
+                    break;
+                }
+            }
+
+            if (symbolDetected) continue;
+
+            _currentText += text[i];
+            label.text = _currentText + (tagOpened? string.Format("</{0}>", tagType) : "");
+            yield return new WaitForSeconds(speed);
+        }
+
+        _typeTextCoroutine = null;
+    }
+
+    private string ReplaceSpeed(string text) {
+        var result = "";
+        var len = text.Length;
+        for (var i = 0; i < len; i++) {
+            if (text[i] == '[' && i + 6 < len && text.Substring(i, 7).Equals("[speed=")) {
+                var speedLength = 0;
+                for (var j = i + 7; j < len; j++) {
+                    if (text[j] == ']')
+                        break;
+                    speedLength++;
+                }
+
+                i += 8 + speedLength - 1;
+                continue;
+            }
+
+            result += text[i];
+        }
+
+        return result;
+    }
+
+    public bool IsSkippable() {
+        return _typeTextCoroutine != null;
+    }
+
+}
+
+public static class TypeTextComponentUtility {
+
+    public static void TypeText(this Text label, string text, float speed = 0.05f) {
+        var typeText = label.GetComponent<TypeTextComponent>();
+        if (typeText == null) {
+            typeText = label.gameObject.AddComponent<TypeTextComponent>();
+        }
+
+        typeText.SetText(text, speed);
+    }
+
+    public static bool IsSkippable(this Text label) {
+        var typeText = label.GetComponent<TypeTextComponent>();
+        if (typeText == null) {
+            typeText = label.gameObject.AddComponent<TypeTextComponent>();
+        }
+
+        return typeText.IsSkippable();
+    }
+
+    public static void SkipTypeText(this Text label) {
+        var typeText = label.GetComponent<TypeTextComponent>();
+        if (typeText == null) {
+            typeText = label.gameObject.AddComponent<TypeTextComponent>();
+        }
+
+        typeText.SkipTypeText();
+    }
+
+}
